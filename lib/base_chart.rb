30464ec40bccf5c6eb380b1d5e969d70b89a692b
Winston Teo <winston.yongwei@gmail.com>
2010-04-20 22:01:44 +0800

    Added a few more data types for values in charts. Thanks to Ohad Levy.

--- lib/base_chart.rb
+++ lib/base_chart.rb
@@ -217,12 +217,12 @@ module GoogleVisualr
 
     end
 
-    # If the column type is 'string'            , the value should be a string.
-    # If the column type is 'number'            , the value should be a number.
-    # If the column type is 'boolean'           , the value should be a boolean.
-    # If the column type is 'date' or 'datetime', the value should be a Date object.
-    # FIXME: To Implement
-    #   - If the column type is 'timeofday'         , the value should be an array of four numbers: [hour, minute, second, millisenconds].
+    # If the column type is 'string'    , the value should be a string.
+    # If the column type is 'number'    , the value should be a number.
+    # If the column type is 'boolean'   , the value should be a boolean.
+    # If the column type is 'date'      , the value should be a Date object.
+    # If the column type is 'datetime'  , the value should be a DateTime or Time object.
+    # If the column type is 'timeofday' , the value should be an array of three or four numbers: [hour, minute, second, optional milliseconds].
     def typecast(value)
 
       case
@@ -230,12 +230,14 @@ module GoogleVisualr
           return "'#{escape_single_quotes(value)}'"
         when value.is_a?(Integer)   || value.is_a?(Float)
           return value
-        when value.is_a?(Date)
-          return "new Date(#{value.year}, #{value.month-1}, #{value.day})"
         when value.is_a?(TrueClass) || value.is_a?(FalseClass)
           return "#{value}"
-        #Time
-          #
+        when value.is_a?(Date)
+          return "new Date(#{value.year}, #{value.month-1}, #{value.day})"
+        when value.is_a?(DateTime)  ||  value.is_a?(Time)
+          return "new Date(#{value.year}, #{value.month-1}, #{value.day}, #{value.hour}, #{value.min}, #{value.sec})"
+        when value.is_a?(Array)
+          return "[#{value.join(',')}]"
         else
           return value
       end
