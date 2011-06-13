8971ef5df8768c05014551c4e4f43dcf042092ac
Winston <winston.yongwei@gmail.com>
2011-06-14 00:30:40 +0800

    Better rakefile and spec_helper.

--- Rakefile
+++ Rakefile
@@ -6,7 +6,7 @@ Bundler::GemHelper.install_tasks
 require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new(:spec) do |t|
-  t.rspec_opts = ['--options', 'spec/spec.opts']
+  t.rspec_opts = ['--options', '.rspec']
 end
 
 task :default => :spec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
-lib_path = File.expand_path("#{File.dirname(__FILE__)}/../lib")
-$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
+require 'rubygems'
+require 'bundler/setup'
 
 require 'google_visualr'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
