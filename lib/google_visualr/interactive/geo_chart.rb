6d2ea188cd37d189081815ca052284ce7aa354dc
Winston <winston.yongwei@gmail.com>
2011-06-30 22:21:16 +0800

    Refresh geo_map to geo_chart.

---
+++ lib/google_visualr/interactive/geo_chart.rb
@@ -0,0 +1,11 @@
+module GoogleVisualr
+  module Interactive
+
+    # http://code.google.com/apis/visualization/documentation/gallery/geochart.html
+    class GeoChart < BaseChart
+      # For Configuration Options, please refer to:
+      # http://code.google.com/apis/visualization/documentation/gallery/geochart.html#Configuration_Options
+    end
+
+  end
+end
