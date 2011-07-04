c36f3421798a1c521ca9e8d4b2960dc1c7f27e23
Winston <winston.yongwei@gmail.com>
2011-07-04 23:11:56 +0800

    Re-add Geomap. Why did I even take it out?

---
+++ lib/google_visualr/interactive/geo_map.rb
@@ -0,0 +1,11 @@
+module GoogleVisualr
+  module Interactive
+
+    # http://code.google.com/apis/chart/interactive/docs/gallery/geomap.html
+    class GeoMap < BaseChart
+      # For Configuration Options, please refer to:
+      # http://code.google.com/apis/chart/interactive/docs/gallery/geomap.html#Configuration_Options
+    end
+
+  end
+end
