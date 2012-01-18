cb57e7c66243c29323139da80eb66ad3220c6e51
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2012-01-18 09:33:34 -0500

    Add support for bar, line, and pie image charts

---
+++ lib/google_visualr/image/image_bar_chart.rb
@@ -0,0 +1,13 @@
+module GoogleVisualr
+  module Image
+
+    # http://code.google.com/apis/chart/interactive/docs/gallery/imagebarchart.html
+    class ImageBarChart < BaseChart
+      include GoogleVisualr::Packages::ImageChart
+
+      # For Configuration Options, please refer to:
+      # http://code.google.com/apis/chart/interactive/docs/gallery/imagebarchart.html#Configuration_Options
+    end
+
+  end
+end
