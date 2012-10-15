a05fcc15e4501a43640ffecb91b1761ac843f5ae
Greg Reinacker <gregr@rassoc.com>
2012-10-15 11:35:37 -0600

    add spec for listener

--- spec/support/common.rb
+++ spec/support/common.rb
@@ -32,6 +32,18 @@ def base_chart_js(div_class="div_class")
 
 end
 
+def base_chart_listener_js(div_class="div_class")
+
+  js  = "\n<script type='text/javascript'>"
+  js << "\n  google.load('visualization','1', {packages: ['basechart'], callback: function() {"
+  js << "\n    var data_table = new google.visualization.DataTable();data_table.addColumn('string', 'Year');data_table.addColumn('number', 'Sales');data_table.addColumn('number', 'Expenses');data_table.addRow([{v: '2004'}, {v: 1000}, {v: 400}]);data_table.addRow([{v: '2005'}, {v: 1200}, {v: 450}]);data_table.addRow([{v: '2006'}, {v: 1500}, {v: 600}]);data_table.addRow([{v: '2007'}, {v: 800}, {v: 500}]);\n    var chart = new google.visualization.BaseChart(document.getElementById('#{div_class}'));"
+  js << "\n    chart.draw(data_table, {legend: 'Test Chart', width: 800, is3D: true});"
+  js << "\n    google.visualization.events.addListener(chart, 'select', function() {test_event(chart);});"
+  js << "\n  }});"
+  js << "\n</script>"
+
+end
+
 # Helpers for image chart specs
 
 def pie_data_table
